3b0743526b61c6572b192506a81ff512c3369faa
Adam Dahan <adamdahan@users.noreply.github.com>
2022-02-08 10:04:33 -0500

    test

--- Package.swift
+++ Package.swift
@@ -3,13 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Wheel",
-    platforms: [.iOS(.v14)],
     products: [
         .library(name: "WheelUI", targets: ["WheelUI"]),
         .library(name: "WheelUIAnimation", targets: ["WheelUIAnimation"])
-    ],
-    dependencies: [
-       //
     ],
     targets: [
         .target(
