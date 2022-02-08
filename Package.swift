60fc413ef89525d12fc84bb36595e470ef7731a1
Adam Dahan <adamdahan@users.noreply.github.com>
2022-02-08 10:45:43 -0500

    trying

--- Package.swift
+++ Package.swift
@@ -1,15 +1,26 @@
-// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Wheel",
     products: [
-        .library(name: "WheelUI", targets: ["WheelUI"]),
-        .library(name: "WheelUIAnimation", targets: ["WheelUIAnimation"])
+        .library(name: "WheelUI",     targets: ["WheelUI"]),
+        .library(name: "WheelUIAnimation",  targets: ["WheelUIAnimation]),
+    ],
+    dependencies: [
+        // SPM does not support mixing of branch-based and unversioned dependencies :(
+        // .package(path: "someComponent"),
+        // .package(path: "anotherComponent"),
     ],
     targets: [
-        .target(
-            name: "Wheel",
-            dependencies: ["WheelUI", "WheelUIAnimation"]),
+        .target(name: "WheelUI", dependencies: [],
+                path: "WheelUI/Sources/"),
+        .testTarget(name: "WheelUITests", dependencies: ["WheelUI"],
+                    path: "WheelUI/Tests/WheelUITests"),
+
+        .target(name: "WheelUIAnimation", dependencies: [],
+                path: "WheelUIAnimation/Sources/"),
+        .testTarget(name: "WheelUIAnimationTests", dependencies: ["WheelUIAnimation"],
+                    path: "WheelUIAnimation/Tests/WheelUIAnimationTests"),
     ]
 )
+
